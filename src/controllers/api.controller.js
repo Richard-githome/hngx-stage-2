fda37f91a25a7560037181bb5022f33b22e588d9
Richard Omeje <chukwuemekaomeje.rich@gmail.com>
2023-09-07 17:53:35 +0100

    resolve date format

--- src/controllers/api.controller.js
+++ src/controllers/api.controller.js
@@ -3,7 +3,7 @@ const {getApiData} = require("../models/api.model");
 function httpGetApiData(req, res){
     const { slack_name, track} = req.query;
     const retrivedData = getApiData(slack_name, track);
-    res.json(retrivedData).status(200);
+    return res.status(200).json({retrivedData});
 }
 
 module.exports = {httpGetApiData};
--- src/models/api.model.js
+++ src/models/api.model.js
@@ -1,30 +1,23 @@
 const dotenv = require('dotenv').config();
 
-const currentDay = new Date().toLocaleString('en-US', { weekday: 'long' });
-const date = new Date();
-const year = date.getUTCFullYear();
-const month = String(date.getUTCMonth() + 1).padStart(2, '0');
-const day = String(date.getUTCDate()).padStart(2, '0');
-const hours = String(date.getUTCHours()).padStart(2, '0');
-const minutes = String(date.getUTCMinutes()).padStart(2, '0');
-const seconds = String(date.getUTCSeconds()).padStart(2, '0');
-
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const currentDay = days[new Date().getDay()];
+const utcTime = new Date().toISOString().slice(0, 19) + "Z";
 
 const apiData = {
     slack_name: "",
     current_day: currentDay,
-    utc_time: `${year}-${month}-${day}T${hours}:${minutes}:${seconds}Z`,
+    utc_time: utcTime,
     track: "backend",
     github_file_url: "https://github.com/Richard-githome/hngx-stage1/blob/main/src/server.js",
     github_repo_url: "https://github.com/Richard-githome/hngx-stage1",
-    status_code: +"",
+    status_code: 200,
 };
 
-function getApiData (slack_name, track){
+const getApiData = (slack_name, track)=>{
     if (slack_name && track){
         apiData.slack_name = slack_name;
         apiData.track = track;
-        apiData.status_code = 200;
     } else if(!slack_name && !track) {
         throw new Error("Invalid query parameters");
     }
