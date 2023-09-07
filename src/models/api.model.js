172d98164d032cb333c186a150ce2f1972966e8d
Richard Omeje <chukwuemekaomeje.rich@gmail.com>
2023-09-07 17:04:30 +0100

    resolve date format

--- src/models/api.model.js
+++ src/models/api.model.js
@@ -1,12 +1,19 @@
 const dotenv = require('dotenv').config();
 
 const currentDay = new Date().toLocaleString('en-US', { weekday: 'long' });
-const currentUTC = new Date().toISOString().slice(0, 19) + 'Z';
+const date = new Date();
+const year = date.getUTCFullYear();
+const month = String(date.getUTCMonth() + 1).padStart(2, '0');
+const day = String(date.getUTCDate()).padStart(2, '0');
+const hours = String(date.getUTCHours()).padStart(2, '0');
+const minutes = String(date.getUTCMinutes()).padStart(2, '0');
+const seconds = String(date.getUTCSeconds()).padStart(2, '0');
+
 
 const apiData = {
     slack_name: "",
     current_day: currentDay,
-    utc_time: currentUTC,
+    utc_time: `${year}-${month}-${day}T${hours}:${minutes}:${seconds}Z`,
     track: "backend",
     github_file_url: "https://github.com/Richard-githome/hngx-stage1/blob/main/src/server.js",
     github_repo_url: "https://github.com/Richard-githome/hngx-stage1",
