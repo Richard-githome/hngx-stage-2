48ab45338e0661cd1268637d55d525cfddd8aff8
Richard Omeje <chukwuemekaomeje.rich@gmail.com>
2023-09-13 10:48:42 +0100

    resolve test script

--- src/test/api.test.js
+++ src/test/api.test.js
@@ -7,13 +7,11 @@ beforeAll(async()=> await mongoConnect());
 afterAll(async()=> await mongoDisconnect());
 
 describe('Testing the CRUD Operations', ()=>{
-//Tests for error handling are yet to be added. Please consider or notice me if it is very needed at this stage of this internship.
-//I'm very open to learning.
 
     describe('POST /api', ()=>{
       test('should return a 201 status code', async()=>{
         const response = await request(app).post('/api')
-        .send({id: 2, name: "Uche"})
+        .send({name: "Uche"})
         .expect(201);
       })
     })
@@ -26,7 +24,7 @@ describe('Testing the CRUD Operations', ()=>{
     });
 
     describe('GET /api/user_id', ()=>{
-      const user_id = 2;
+      const user_id = 1;
       test('should return a 200 status code', async()=>{
         const response = await request(app).get(`/api/${user_id}`)
         .expect(200);
@@ -34,7 +32,7 @@ describe('Testing the CRUD Operations', ()=>{
     });
 
     describe('UPDATE | PATCH /api/user_id', ()=>{
-      const user_id = 2;
+      const user_id = 1;
       test('should return a 200 status code', async()=>{
         const response = await request(app).patch(`/api/${user_id}`)
         .send({ name: "Ebuka" })
@@ -43,13 +41,11 @@ describe('Testing the CRUD Operations', ()=>{
     });
 
     describe('DELETE /api/user_id', ()=>{
-      const user_id = 2;
+      const user_id = 1;
       test('should return a 200 status code', async()=>{
         const response = await request(app).delete(`/api/${user_id}`)
         .expect(200);
       });
     });
 
-    //Tests for error handling are yet to be added. Please consider or notice me if it is very needed at this stage of this internship.
-    //I'm very open to learning.
 })
